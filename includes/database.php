1a0cd1d9915d771817f12a2570e7e8c805cccdd3
Andrés Murillo <andresdavid1305@hotmail.com>
2023-08-31 13:34:28 -0500

    barberia

---
+++ includes/database.php
@@ -0,0 +1,11 @@
+<?php
+$db->set_charset("utf8");
+$db = mysqli_connect('localhost', 'root', 'admin', 'appsalon');
+
+if (!$db) {
+    echo "Error: No se pudo conectar a MySQL.";
+    echo "errno de depuración: " . mysqli_connect_errno();
+    echo "error de depuración: " . mysqli_connect_error();
+    exit;
+}
+
